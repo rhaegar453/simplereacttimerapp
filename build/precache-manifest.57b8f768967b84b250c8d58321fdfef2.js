d38f396064eebbd3035beb585e6de2e43b53f17d
rhaegar453 <shivarajapple@gmail.com>
2018-12-13 19:24:31 +0530

    First Commit

---
+++ build/precache-manifest.57b8f768967b84b250c8d58321fdfef2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "188821e56f88a25bccb6",
+    "url": "/static/js/main.188821e5.chunk.js"
+  },
+  {
+    "revision": "846e098a801d8cb491f7",
+    "url": "/static/js/1.846e098a.chunk.js"
+  },
+  {
+    "revision": "188821e56f88a25bccb6",
+    "url": "/static/css/main.64421bdc.chunk.css"
+  },
+  {
+    "revision": "4b7d613db1636f372e3341572af87b9d",
+    "url": "/index.html"
+  }
+];
